c058ffe50cc5e3d06c41178cce247a6f7dfe738d
Yashwardhan <yashwardhan.student.che21@itbhu.ac.in>
2024-05-19 16:32:36 +0530

    iii

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     credentials: true,
-    origin: process.env.CLIENT_URL,
+    origin: '*',
 }));
 
 const db = mongoose.connection;
